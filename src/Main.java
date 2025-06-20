f0b38b4beae58ba2c2628c25393655465ff703ce
SUNG-JIN-KIM <ksjcloud98@gmail.com>
2025-06-20 14:34:46 +0900

    Runnable SumR

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,45 @@
+class SumR implements Runnable{
+
+    private final int[] numbers;
+
+    public SumR(int[] numbers){
+        this.numbers = numbers;
+    }
+
+    @Override
+    public void run(){
+
+        int sum = 0;
+
+        for(int n : numbers){
+            sum += n;
+
+            try{
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                System.out.println(e.getMessage());
+            }
+        }
+
+        System.out.println(Thread.currentThread().getName() + " - Sum: " + sum);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        Thread thread = new Thread(() -> {
-            for (int i = 1; i <= 5; i++) {
-                System.out.println("Hello from Thread: " + i);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    System.out.println(e.getMessage());
-                }
-            }
-        }, "CountThread");
-
-        thread.start();
+        int[][] dataSets = {
+                {1, 2, 3, 4, 5},
+                {100, 200, 300, 400},
+                {10, 20, 30},
+                {100, 200, 300, 400},
+                {7, 14, 21, 28},
+                {100, 200, 300, 400},
+                {100, 200, 300, 400}
+        };
+        for(int i = 0; i < dataSets.length; i++){
+            Thread t = new Thread(new SumR(dataSets[i]), "Thread-" + (i + 1));
+            t.start();
+        }
+        System.out.println("All sum threads started.");
     }
 }

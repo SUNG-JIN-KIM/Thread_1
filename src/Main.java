649ad66a9d4c53941b19ac034d85693fde6714a2
SUNG-JIN-KIM <ksjcloud98@gmail.com>
2025-06-20 13:23:50 +0900

    Runnable

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,23 @@
-class MyThread extends Thread{
-    @Override
-    public void run(){
-        for(int i = 0; i <= 5; i++){
-            System.out.print(i + " ");
+class MyRunnable implements Runnable{
 
-            try{
-                Thread.sleep(5000);
-            }catch (InterruptedException e){
-                System.out.println(e.getMessage());
+    public void run(){
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hello from Thread: " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread was interrupted.");
             }
         }
     }
 }
+
 public class Main {
     public static void main(String[] args) {
-        Thread t = new MyThread();
-        t.start();
+        Thread thread = new Thread(new MyRunnable(), "CountThread");
+
+        thread.setName("안녕하슈");
+
+        thread.start();
     }
 }

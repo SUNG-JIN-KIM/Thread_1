85a92c05f6b8694dca35424bfb94aeeea20b4d80
SUNG-JIN-KIM <ksjcloud98@gmail.com>
2025-06-20 13:34:32 +0900

    Runnable Lambda

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,15 @@
-class MyRunnable implements Runnable{
-
-    public void run(){
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Hello from Thread: " + i);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                System.out.println("Thread was interrupted.");
-            }
-        }
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        Thread thread = new Thread(new MyRunnable(), "CountThread");
-
-        thread.setName("안녕하슈");
+        Thread thread = new Thread(() -> {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("Hello from Thread: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }, "CountThread");
 
         thread.start();
     }
